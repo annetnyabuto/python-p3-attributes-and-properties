f496d2049ae6d80c0eb292d84e5454e3311cb02d
annetnyabuto <annetgnyabuto@gmail.com>
2025-08-17 01:44:01 +0300

    attributes and properties

--- lib/person.py
+++ lib/person.py
@@ -2,7 +2,7 @@
 
 APPROVED_JOBS = [
     "Admin",
-    "Customer Service",
+    "Customer Service", 
     "Human Resources",
     "ITC",
     "Production",
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Guido", job="ITC"):
+            self.name = name
+            self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in APPROVED_JOBS or job == "":
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
